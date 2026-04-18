2b77583c5d670d6a6e7589eabf909c732187d193
nyny567567 <ny567@naver.com>
2026-04-19 00:42:20 +0900

    2026.04.18

---
+++ src/variable/Var1.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var1 {
+    static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
---
+++ src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+    static void main(String[] args) {
+        //정수
+        byte b = 127; //-128 ~ 127
+        short s = 32767; //-32768 ~ 32767
+        int i = 2147483647; //-2147483648 ~ 2147483647
+
+        //-9223372036854775808 ~ 9223372036854775807
+        long l = 9223372036854775807L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
